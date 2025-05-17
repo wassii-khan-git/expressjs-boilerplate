a68a3fc5c048e2d240c9b0bfac1ca45ac9ba6642
wassii-khan <wassiikhan933@gmail.com>
2025-05-17 21:47:51 +0500

    Initialize Express server with CORS and routing setup

---
+++ index.js
@@ -0,0 +1,29 @@
+// express
+import express from "express";
+// third party
+import cors from "cors";
+// routes
+import router from "./routes/index.js";
+// custom
+import DatabaseConnection from "./db/connection.js";
+import { PORT_NUMBER } from "./config/config.js";
+
+const app = express();
+// database connection
+DatabaseConnection();
+
+app.use(cors());
+app.use(express.json());
+
+// HTTP routes still work
+app.get("/", (req, res) => {
+  res.send("<h1>Server is healthy</h1>");
+});
+
+// Use your existing routes:
+app.use("/v1/api", router);
+
+// server listens on port number
+app.listen(PORT_NUMBER, () => {
+  console.log(`Server is running on port ${PORT_NUMBER}`);
+});
